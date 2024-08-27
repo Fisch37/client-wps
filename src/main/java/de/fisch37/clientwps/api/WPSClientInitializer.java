66328a52918e9336af15d88f4f7f4ecae6979f9b
Fisch37 <seracin4@gmail.com>
2024-08-27 11:01:36 +0200

    Added API documentation

--- src/main/java/de/fisch37/clientwps/api/WPSClientInitializer.java
+++ src/main/java/de/fisch37/clientwps/api/WPSClientInitializer.java
@@ -6,11 +6,28 @@ import de.fisch37.clientwps.packet.waypoints.WaypointInfo;
 import java.util.List;
 
 public interface WPSClientInitializer {
+    /**
+     * Called when the player has joined a server (regardless whether the server has cWPS installed)
+     * @param sender Object used to send commands to the server
+     */
     void onCWPSInitialize(WPSSender sender);
 
+    /**
+     * The features this entrypoint requires.
+     * Note that not enabling a feature does not guarantee its respective methods won't be called.
+     * @return The features supported by this entrypoint
+     */
     ClientFeaturesPayload getFeatures();
 
+    /**
+     * Called when the server sends waypoints.
+     * @param waypoints The received {@link WaypointInfo} objects
+     */
     default void onWaypoints(List<WaypointInfo> waypoints) { }
 
+    /**
+     * Called when a waypoint has changed
+     * @param waypoint The new {@link WaypointInfo} object
+     */
     default void onWaypointUpdate(WaypointInfo waypoint) { }
 }
--- src/main/java/de/fisch37/clientwps/api/WPSSender.java
+++ src/main/java/de/fisch37/clientwps/api/WPSSender.java
@@ -3,5 +3,11 @@ package de.fisch37.clientwps.api;
 import de.fisch37.clientwps.data.WaypointKey;
 
 public interface WPSSender {
+    /**
+     * Teleport the player to the given waypoint
+     * @implNote Note that this method does nothing if the player can't access the waypoint.
+     *  Admins should be teleported using a /tp instead.
+     * @param target The Waypoint to teleport to
+     */
     void teleport(WaypointKey target);
 }
