c8cbdde9c797be122146c7f765a0bde070fbad1e
Fisch37 <seracin4@gmail.com>
2024-08-30 20:02:30 +0200

    Actually added the listeners
    oops

--- src/main/java/de/fisch37/clientwps/ClientWps.java
+++ src/main/java/de/fisch37/clientwps/ClientWps.java
@@ -6,8 +6,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientWps implements ClientModInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger("cWPS Client");
+    private WPSClientInitializer initializer;
+
     @Override
     public void onInitializeClient() {
         PacketTypes.register();
@@ -15,11 +20,22 @@ public class ClientWps implements ClientModInitializer {
             FabricLoader.getInstance().getEntrypoints("cwps", WPSClientInitializer.class)
                     .forEach(this::clientInit);
         });
+        registerReceivers();
+        LOGGER.info("ClientWps initialized");
     }
 
     private void clientInit(WPSClientInitializer initializer) {
+        this.initializer = initializer;
         initializer.onCWPSInitialize(new WPSSenderImpl());
         ClientPlayNetworking.send(initializer.getFeatures());
+    }
 
+    private void registerReceivers() {
+        ClientPlayNetworking.registerGlobalReceiver(PacketTypes.WAYPOINTS, ((payload, context) -> {
+            initializer.onWaypoints(payload.waypoints());
+        }));
+        ClientPlayNetworking.registerGlobalReceiver(PacketTypes.WAYPOINT_UPDATE, ((payload, context) -> {
+            initializer.onWaypointUpdate(payload.waypointInfo());
+        }));
     }
 }
